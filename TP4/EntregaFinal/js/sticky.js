f80e717cbc7eb0168797c810a836bda49d8564e8
Blas Echandi <bechandi@alumnos.exa.unicen.edu.ar>
2024-11-17 18:29:53 -0300

    añadido transparencia al header

--- TP4/EntregaFinal/js/sticky.js
+++ TP4/EntregaFinal/js/sticky.js
@@ -1,6 +1,7 @@
 window.addEventListener("scroll", function() {
     const logo = document.getElementById("logo_esconder");
     const logoChiquito  = document.getElementById("logo-chico");
+    let header = document.querySelector("header");
 
     const scrollY = window.scrollY;
 
@@ -8,8 +9,10 @@ window.addEventListener("scroll", function() {
     if (scrollY > 100) {
         logo.classList.add("achicar");
         logoChiquito.classList.add("logo-small-block");
+        header.style.background = "#00D0D5";
     } else {
         logo.classList.remove("achicar");
         logoChiquito.classList.remove("logo-small-block");
+        header.style.background = "transparent";
     }
 });
