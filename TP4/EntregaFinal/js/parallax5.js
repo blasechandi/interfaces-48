be15aff68249c4ce2c286e85dab2e1b75b163d77
Blas Echandi <bechandi@alumnos.exa.unicen.edu.ar>
2024-11-21 18:49:20 -0300

    TP4 finalizado

--- TP4/EntregaFinal/js/parallax5.js
+++ TP4/EntregaFinal/js/parallax5.js
@@ -19,44 +19,45 @@ let img10 = document.querySelector('.img10');
 /*cambio de las imagenes del parallax5, a medida que se scrollea los textos*/
 txtS.addEventListener('scroll', function(){
     let scrollTop = txtS.scrollTop;
+    console.log(scrollTop);
 
-    if(scrollTop <= 270){  
+    if(scrollTop <= 300){  
         img0.style.opacity = 1;
         img1.style.opacity = 0;
 
-    }else if(scrollTop > 270 && scrollTop <= 610){
+    }else if(scrollTop > 300 && scrollTop <= 610){
         img0.style.opacity = 0;
         img1.style.opacity = 1;
         img2.style.opacity = 0;
-    }else if(scrollTop > 610 && scrollTop <= 900){
+    }else if(scrollTop > 610 && scrollTop <= 918){
         img1.style.opacity = 0;
         img2.style.opacity = 1;
         img3.style.opacity = 0;
-    }else if(scrollTop > 900 && scrollTop <= 1322){
+    }else if(scrollTop > 918 && scrollTop <= 1430){
         img2.style.opacity = 0;
         img3.style.opacity = 1;
         img4.style.opacity = 0;
-    }else if(scrollTop > 1322 && scrollTop <= 1600){
+    }else if(scrollTop > 1430 && scrollTop <= 1938){
         img3.style.opacity = 0;
         img4.style.opacity = 1;
         img5.style.opacity = 0;
-    }else if(scrollTop > 1601 && scrollTop <= 1930){
+    }else if(scrollTop > 1938 && scrollTop <= 2244){
         img4.style.opacity = 0;
         img5.style.opacity = 1;
         img6.style.opacity = 0;
-    }else if(scrollTop > 1931 && scrollTop <= 2200){
+    }else if(scrollTop > 2244 && scrollTop <= 2754){
         img5.style.opacity = 0;
         img6.style.opacity = 1;
         img7.style.opacity = 0;
-    }else if(scrollTop > 2201 && scrollTop <= 2500){
+    }else if(scrollTop > 2754 && scrollTop <= 3162){
         img6.style.opacity = 0;
         img7.style.opacity = 1;
         img8.style.opacity = 0;
-    }else if(scrollTop > 2501 && scrollTop <= 2800){
+    }else if(scrollTop > 3162 && scrollTop <= 3604){
         img7.style.opacity = 0;
         img8.style.opacity = 1;
         img9.style.opacity = 0;
-    }else if(scrollTop > 2801 && scrollTop <= 3100){
+    }else if(scrollTop > 3604 && scrollTop <= 3974){
         img8.style.opacity = 0;
         img9.style.opacity = 1;
         img10.style.opacity = 0;
